a1b393ebb2f6415718c0e8df07cb2ca4ba88a7cb
Chen Ye <blasternt@outlook.com>
2017-10-05 23:34:55 +0000

    start three

--- src/index.js
+++ src/index.js
@@ -1,1 +1,8 @@
-alert('hi')
+import { Scene, PerspectiveCamera, WebGLRenderer } from 'three';
+
+var scene = new Scene();
+var camera = new PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+
+var renderer = new WebGLRenderer();
+renderer.setSize( window.innerWidth, window.innerHeight );
+document.body.appendChild( renderer.domElement );

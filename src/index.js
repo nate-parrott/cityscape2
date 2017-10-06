2aa19ba6ba8992f766c507fddf290b33fc4ee4f9
Chen Ye <blasternt@outlook.com>
2017-10-06 00:56:48 +0000

    network rendering

--- src/index.js
+++ src/index.js
@@ -1,14 +1,80 @@
-import { Scene, PerspectiveCamera, WebGLRenderer } from 'three';
+import { 
+    WebGLRenderer, 
+    Mesh,
+    Line,
+    BufferGeometry,
+    BufferAttribute,
+    BoxBufferGeometry, 
+    MeshBasicMaterial,
+    LineBasicMaterial,
+    GridHelper,
+} from 'three';
+import * as THREE from 'three';
+self.THREE = THREE;
+require("three/examples/js/controls/OrbitControls");
 
-var scene = new Scene();
-var camera = new PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+camera.position.z = 5;
+const controls = new THREE.OrbitControls( camera );
+window.onresize = function () {
+	camera.aspect = window.innerWidth / window.innerHeight;
+	camera.updateProjectionMatrix();
+	renderer.setSize( window.innerWidth, window.innerHeight );
+};
 
-var renderer = new WebGLRenderer();
+const renderer = new WebGLRenderer();
 renderer.setSize( window.innerWidth, window.innerHeight );
 document.body.appendChild( renderer.domElement );
 
-self.fetch('city2.json').then((response) => {
-    return response.json();
-}).then((json) => {
-    console.log(json);
-})
+const geometry = new BoxBufferGeometry( 0.1, 0.1, 0.1 );
+const material = new MeshBasicMaterial( { color: 0xffffff } );
+const lineMaterial = new LineBasicMaterial({ color: 0x0000ff });
+
+const grid = new GridHelper( 100, 100 );
+grid.rotation.x = Math.PI / 2;
+grid.position.z = -0.1;
+scene.add(grid);
+
+const load = () => {
+    self.fetch('city2.json').then((response) => {
+        return response.json();
+    }).then(drawState);
+}
+
+const drawState = (state) => {
+    const network = state.map.network;
+    for(const nodeID in network.nodes) {
+        const node = network.nodes[nodeID];
+        const cube = new Mesh( geometry, material );
+        cube.position.x = node.coordinate.x;
+        cube.position.y = node.coordinate.y;
+        scene.add(cube);
+    }
+    for(const edgeId in network.edges) {
+        const edge = network.edges[edgeId];
+        const endNode = network.nodes[edge.endId];
+        const startNode = network.nodes[edge.startId];
+        
+        if (startNode && endNode) {
+            const geometry = new BufferGeometry();
+            const vertices = new Float32Array([
+                startNode.coordinate.x, startNode.coordinate.y, 0,
+                endNode.coordinate.x, endNode.coordinate.y, 0,
+            ]);
+            geometry.addAttribute( 'position', new BufferAttribute( vertices, 3 ) );
+            
+            const edgeLine = new Line( geometry, lineMaterial );
+            scene.add(edgeLine);
+        }
+    }
+    console.log(state);
+}
+
+const animate = () => {
+	requestAnimationFrame( animate );
+	renderer.render( scene, camera );
+}
+
+load();
+animate();

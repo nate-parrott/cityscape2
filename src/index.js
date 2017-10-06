da86aed35e1865c11a52663c92bcf23c606f5deb
Chen Ye <blasternt@outlook.com>
2017-10-06 01:34:47 +0000

    render agents

--- src/index.js
+++ src/index.js
@@ -2,11 +2,6 @@ import {
     WebGLRenderer, 
     Mesh,
     Line,
-    BufferGeometry,
-    BufferAttribute,
-    BoxBufferGeometry, 
-    MeshBasicMaterial,
-    LineBasicMaterial,
     GridHelper,
 } from 'three';
 import * as THREE from 'three';
@@ -17,6 +12,7 @@ const backgroundColor = 0xffffff;
 const networkColor = 0x6F7A90;
 const residentialColor = 0xB3D28A;
 const commercialColor = 0xADD1FC;
+const personColor = 0xB5A8BA;
 
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
@@ -35,13 +31,15 @@ renderer.setSize( window.innerWidth, window.innerHeight );
 renderer.setClearColor( backgroundColor, 1 );
 document.body.appendChild( renderer.domElement );
 
-const networkNodeGeometry = new BoxBufferGeometry( 0.1, 0.1, 0.1 );
-const buildingGeometry = new BoxBufferGeometry( 0.2, 0.2, 0.2 );
+const networkNodeGeometry = new THREE.BoxBufferGeometry( 0.1, 0.1, 0.1 );
+const buildingGeometry = new THREE.BoxBufferGeometry( 0.2, 0.2, 0.2 );
+const personGeometry = new THREE.DodecahedronBufferGeometry( 0.1, 0 );
 
-const commercialBuildingMaterial = new MeshBasicMaterial( { color: commercialColor } );
-const residentialBuildingMaterial = new MeshBasicMaterial( { color: residentialColor } );
-const networkNodeMaterial = new MeshBasicMaterial( { color: networkColor } );
-const networkEdgeMaterail = new LineBasicMaterial({ color: networkColor });
+const commercialBuildingMaterial = new THREE.MeshBasicMaterial( { color: commercialColor } );
+const residentialBuildingMaterial = new THREE.MeshBasicMaterial( { color: residentialColor } );
+const networkNodeMaterial = new THREE.MeshBasicMaterial( { color: networkColor } );
+const networkEdgeMaterial = new THREE.LineBasicMaterial({ color: networkColor });
+const personMaterial = new THREE.MeshBasicMaterial({ color: personColor });
 
 const buildingTypes = {
     "work": {
@@ -63,9 +61,17 @@ const load = () => {
     }).then(drawState);
 }
 
+const lerpCoordinates = ( coordinateA, coordinateB, progress ) => {
+    return {
+        x: coordinateA.x + (coordinateB.x - coordinateA.x) * progress,
+        y: coordinateA.y + (coordinateB.y - coordinateA.y) * progress,
+    }
+}
+
 const drawState = (state) => {
     const network = state.map.network;
     const buildings = state.map.buildings;
+    const people = state.agents.people;
     for(const nodeID in network.nodes) {
         const node = network.nodes[nodeID];
         const nodeMesh = new Mesh( networkNodeGeometry, networkNodeMaterial );
@@ -79,14 +85,14 @@ const drawState = (state) => {
         const startNode = network.nodes[edge.startId];
         
         if (startNode && endNode) {
-            const edgeGeometry = new BufferGeometry();
+            const edgeGeometry = new THREE.BufferGeometry();
             const vertices = new Float32Array([
                 startNode.coordinate.x, startNode.coordinate.y, 0,
                 endNode.coordinate.x, endNode.coordinate.y, 0,
             ]);
-            edgeGeometry.addAttribute( 'position', new BufferAttribute( vertices, 3 ) );
+            edgeGeometry.addAttribute( 'position', new THREE.BufferAttribute( vertices, 3 ) );
             
-            const edgeLine = new Line( edgeGeometry, networkEdgeMaterail );
+            const edgeLine = new Line( edgeGeometry, networkEdgeMaterial );
             scene.add(edgeLine);
         }
     }
@@ -96,6 +102,20 @@ const drawState = (state) => {
         buildingMesh.position.x = building.coordinate.x;
         buildingMesh.position.y = building.coordinate.y;
         scene.add(buildingMesh);
+    }
+    for(const personID in people) {
+        const person = people[personID];
+        const edge = network.edges[person.position.edgeId];
+        const endNode = network.nodes[edge.endId];
+        const startNode = network.nodes[edge.startId];
+        
+        const coordinate = lerpCoordinates(startNode.coordinate, endNode.coordinate, person.position.distance);
+        
+        const personMesh = new Mesh( personGeometry, personMaterial );
+        personMesh.position.x = coordinate.x;
+        personMesh.position.y = coordinate.y;
+        
+        scene.add(personMesh);
     }
     console.log(state);
 }

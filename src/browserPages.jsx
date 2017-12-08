1f0960357fbc3b8815e526ebb697d7fbe98281e5
Nate Parrott <nateparro2t@gmail.com>
2017-12-07 22:46:01 -0500

    roomster

--- src/browserPages.jsx
+++ src/browserPages.jsx
@@ -39,22 +39,50 @@ let FriendCell = ({id, agent, navigate}) => {
 
 let FriendsterProfile = ({agentId, navigate, simState}) => {
 	let agent = simState.agents.people[agentId];
+	let apartment = agent.homeId ? <ApartmentCell id={agent.homeId} simState={simState} navigate={navigate} /> : <EmptyState>No home</EmptyState>;
 	return (
 		<div className='FriendsterProfile friendster-pages-shared'>
 			<img src='/webAssets/blankProfile.jpg' />
 			<h1>{agent.name}</h1>
 			<p>{agent.ageYears} years old</p>
+			<h3>Residence:</h3>
+			{apartment}
 		</div>
 	)
 }
 
 export let RoomsterPage = ({navigate, simState}) => {
+	let buildingsById = simState.map.buildings;
+	let apartmentIds = Object.keys(buildingsById).filter((id) => buildingsById[id].typeId === 'home');
 	return (
 		<div className='RoomsterPage roomster-pages-shared'>
 			<header>
 				<img src='/webAssets/Roomster.png' />
 				<p>Live where you love. Love where you live. Love where you love.</p>
 			</header>
+			<ul>{ apartmentIds.map((id) => <li key={id}><ApartmentCell id={id} navigate={navigate} simState={simState} /></li> ) }</ul>
 		</div>
 	)
+}
+
+let ApartmentCell = ({id, navigate, simState}) => {
+	let building = simState.map.buildings[id];
+	return (
+		<div className='ApartmentCell'>
+			<h3>{building.name}</h3>
+			<div className='details'>
+				<ApartmentDetail name="Rent: " value={building.rent} />
+				<ApartmentDetail name="Occupants: " value={building.occupants.length} />
+				<ApartmentDetail name="Vacancies: " value={building.occupancy - building.occupants.length} />
+			</div>
+		</div>
+	)
+}
+
+let ApartmentDetail = ({name, value}) => {
+	return <div className='ApartmentDetail'><span className='name'>{name}</span><span className='value'>{value}</span></div>;
+}
+
+let EmptyState = ({children}) => {
+	return <p className='EmptyState'>{children}</p>;
 }

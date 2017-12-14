e8c76e2d05795214c5a4dcea54607d2025c90cfe
Nate Parrott <nateparro2t@gmail.com>
2017-12-13 20:41:52 -0500

    calzones and guns

--- src/generateNames.js
+++ src/generateNames.js
@@ -18,7 +18,6 @@ let streetNames = [
 	"Brown St"
 ]
 
-
 export let personName = () => {
 	return pick1(names) + ' ' + pick1(names);
 }
@@ -27,6 +26,74 @@ export let apartmentName = () => {
 	return ((Math.random() * 1200) | 0) + ' ' + pick1(streetNames);
 }
 
+let nameGen = (options) => {
+	let picked = pick1(options);
+	if (typeof(picked) === 'string') return picked;
+	return picked.map((p) => {
+		if (typeof(p) === 'function') {
+			return nameGen(p());
+		} else {
+			return p;
+		}
+	}).join('');
+}
+
+
 export let businessName = () => {
-	return 'K-Mart';
+	let letters = () => {
+		return ['A', 'B', 'K', 'H', 'Z'];
+	}
+	let specialty = () => {
+		return [
+			'Party',
+			'Shoe',
+			'Food',
+			'Knicknack',
+			'Pet',
+			'Beer',
+			'Dance',
+			'Barber',
+			'Book'
+		];
+	}
+	let names = () => {
+		return [
+			"Mike's",
+			"Tom's",
+			"Stacy's",
+			"Joe's",
+			"Pat's",
+			"Chen's",
+			"Willy's"
+		]
+	}
+	let things = () => {
+		return [
+			"Calzones",
+			"Pizza",
+			"Chowder",
+			"Guns",
+			"Market",
+			"Books",
+			"Coffee"
+		]
+	};
+	let suffix = () => {
+		return [
+			'-Mart',
+			' City',
+			'Zone',
+			' Haus',
+			' Shack',
+			' Center'
+		]
+	}
+	let bizOptions = [
+		[letters, '-Mart'],
+		[specialty, suffix],
+		[names, ' ', things],
+		[things, ' and ', things],
+		[things, suffix]
+	];
+	return nameGen(bizOptions);
 }
